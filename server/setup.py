8cb4f4484309143a67d22ef2a661c07f96ff13a1
Rodrigo Frazão Maia <rodrigo.frazao@gmail.com>
2020-07-02 12:38:28 -0300

    refact: adjust requirements

---
+++ server/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="fusao-server",
+    version="0.1.0",
+    description="",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["flask"],
+)
